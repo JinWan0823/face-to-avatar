80b6f1f6ecc11202971c473d81a2ea76c3a00445
jinWan0823 <dhksl823@gmail.com>
2025-08-08 17:46:07 +0900

    ✨ 유저 로그인 기능 구현

--- src/_components/common/InputType.tsx
+++ src/_components/common/InputType.tsx
@@ -1,14 +1,25 @@
+import { SetStateAction } from "react";
+
 interface InputProps {
   type: string;
   placeholder?: string;
+  value: string;
+  onChange: React.Dispatch<SetStateAction<string>>;
 }
 
-export default function InputType({ type, placeholder }: InputProps) {
+export default function InputType({
+  type,
+  placeholder,
+  value,
+  onChange,
+}: InputProps) {
   return (
     <input
       type={type}
       className="w-full p-2 px-2 border-1 border-[#dfdfdf] outline-[#da6319] rounded"
       placeholder={placeholder}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
     />
   );
 }
