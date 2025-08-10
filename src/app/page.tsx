f8a3af4f8735cfec8ae395025a8cb4705975b521
jinWan0823 <dhksl823@gmail.com>
2025-08-11 04:42:47 +0900

    ✨ 공용 헤더 + 로그아웃 기능 추가 , 메인페이지 레이아웃 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,37 @@
 import { checkUser } from "@/_lib/checkUser";
+import { GoPlus } from "react-icons/go";
 
 export default async function Home() {
   await checkUser();
   return (
     <>
-      <main
-        className="w-full max-w-[460px] 
-          mx-auto bg-[#fdf2e3]"
-      >
-        <div
-          className="inner w-[95%] min-h-[100vh] mx-auto py-10 
-          flex flex-col items-center justify-center"
-        ></div>
-      </main>
+      <div className="inner w-[95%] min-h-[100vh] mx-auto pt-24 py-8">
+        <div>
+          <p className="font-bold">
+            <span className="text-2xl text-[#da6319] underline">
+              듀두듀듀님
+            </span>{" "}
+            새로운 AI 캐릭터를 만들어보세요!
+          </p>
+          <ul className="mt-2 flex items-center gap-2 overflow-x-auto whitespace-nowrap w-max">
+            <li
+              className="w-[110px] h-[160px] rounded border-2 border-[#da6319] bg-[#fff]
+              flex items-center justify-center"
+            >
+              <GoPlus className="text-3xl text-[#da6319]" />
+            </li>
+            <li className="w-[110px] h-[160px] rounded bg-[#333]"></li>
+            <li className="w-[110px] h-[160px] rounded bg-[#333]"></li>
+            <li className="w-[110px] h-[160px] rounded bg-[#333]"></li>
+            <li className="w-[110px] h-[160px] rounded bg-[#333]"></li>
+            <li className="w-[110px] h-[160px] rounded bg-[#333]"></li>
+          </ul>
+        </div>
+
+        <div className="mt-10 text-xl font-bold">
+          <p>다른 사람들이 만든 캐릭터</p>
+        </div>
+      </div>
     </>
   );
 }
