6b51e00a772e49438a4908e28874fe58c3165c53
jinWan0823 <dhksl823@gmail.com>
2025-09-03 10:15:25 +0900

    ✨ 이미지 생성 POST 요청 API 구현

--- src/app/api/story/route.ts
+++ src/app/api/story/route.ts
@@ -1,11 +1,10 @@
 import { connectDB } from "@/_lib/mongodb";
 import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../auth/[...nextauth]/authOptions";
 
 export async function GET() {
   const session = await getServerSession(authOptions);
-  console.log(session?.user.username);
   if (!session || !session.user) {
     return NextResponse.json(
       { message: "로그인 정보가 필요합니다." },
@@ -31,11 +30,44 @@ export async function GET() {
   }
 }
 
-// export async function POST(){
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return NextResponse.json(
+      { message: "로그인 정보가 필요합니다." },
+      { status: 400 }
+    );
+  }
 
-//   try{
+  const data = await req.json();
+  const { imgFile, prompt, keywords } = data;
 
-//   } catch(error){
+  if (!imgFile || !prompt || !keywords) {
+    return NextResponse.json(
+      { message: "필수 입력란을 확인해주세요." },
+      { status: 400 }
+    );
+  }
+  try {
+    const client = await connectDB;
+    const db = client.db("heroMe");
 
-//   }
-// }
+    const createData = await db.collection("createData").insertOne({
+      username: session.user.username,
+      image: imgFile,
+      prompt: prompt,
+      keywords: keywords,
+      createdAt: new Date(),
+    });
+
+    return NextResponse.json(
+      { message: "데이터 등록 성공", createData },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "서버 에러 발생", error: String(error) },
+      { status: 500 }
+    );
+  }
+}
