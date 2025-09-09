4eb5ba8b598de0d63b4f32fd85009ed3a85bfb83
jinWan0823 <dhksl823@gmail.com>
2025-09-09 12:20:05 +0900

    📝 잘못 입력된 코드

--- src/app/(route)/create/page.tsx
+++ src/app/(route)/create/page.tsx
@@ -1,6 +1,6 @@
 import ImageCreateWrap from "@/_components/create/ImageCreateWrap";
 
-export default async function CreatePage() {
+export default function CreatePage() {
   return (
     <>
       <div className="inner w-[95%] min-h-[100vh] mx-auto py-8">
