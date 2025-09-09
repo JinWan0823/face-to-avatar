0d23ec4535c308f2b8cd937ac0cdc07929f9455a
jinWan0823 <dhksl823@gmail.com>
2025-09-09 12:02:20 +0900

    📝 api 경로 수정

--- src/_hooks/useSignUp.tsx
+++ src/_hooks/useSignUp.tsx
@@ -29,7 +29,7 @@ export default function useSignUp() {
         return;
       }
 
-      const res = await fetch("/api/auth/signup", {
+      const res = await fetch("/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
